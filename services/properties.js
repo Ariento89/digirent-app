44a2f411d7298b6a7bd4d451505db5b0389da56f
Shaibu Shaibu <s.shaibu.jnr@gmail.com>
2021-02-28 14:42:45 +0100

    define services for apple auth

--- services/properties.js
+++ services/properties.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { HEADER_MULTIPART_FORM_DATA } from './index';
 
 export const service = {
-  fetchProperties: async (params) => axios.get('/apartments', { params }),
-  createProperty: async (body) => axios.post('/apartments', body),
+  fetchProperties: async (params) => axios.get('/apartments/', { params }),
+  createProperty: async (body) => axios.post('/apartments/', body),
   getProperty: async (propertyId) => axios.get(`/apartments/${propertyId}`),
   updateProperty: async (propertyId, body) => axios.put(`/apartments/${propertyId}`, body),
   uploadImage: async (propertyId, body) => axios.post(`/apartments/${propertyId}/images`, body, HEADER_MULTIPART_FORM_DATA),
